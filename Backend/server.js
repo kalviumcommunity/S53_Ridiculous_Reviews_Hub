4ad643d9a235b2e75a0fed638f17b6ace9f95244
qnasir <qnasir575@gmail.com>
2024-02-13 15:08:45 +0530

    cors

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require("cors")
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors())
 
 // Middleware for error catching
 app.use((err, req, res, next) => {
