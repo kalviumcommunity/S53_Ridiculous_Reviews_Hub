b0a311388e81189dd39117bc744952ab7bb497fb
qnasir <qnasir575@gmail.com>
2024-02-13 15:05:41 +0530

    Updated Entity

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors())
 
 
 // Middleware for error catching
